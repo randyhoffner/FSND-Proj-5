a58380349ea82b9e2f70af0468b9406f5ac28cdb
Randy Hoffner <rnh@ashlandoregon.org>
2017-04-22 23:31:56 -0700

    Add files via upload

--- database_setup.py
+++ database_setup.py
@@ -3,6 +3,7 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -40,7 +41,7 @@ class MenuItem(Base):
 	id = Column(Integer, primary_key=True)
 	description = Column(String(250))
 	category_id = Column(Integer, ForeignKey('category.id'))
-	category = relationship(Category)
+	category = relationship(Category, backref=backref('menu_item', cascade='all, delete'))
 	user_id = Column(Integer, ForeignKey('user.id'))
 	user = relationship(User)
 
